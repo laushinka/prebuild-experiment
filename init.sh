1735b188c2746ec9c087979213de977383f92697
Kyle Brennan <kylos101@gmail.com>
2022-02-28 21:31:25 +0000

    Moar

--- init.sh
+++ init.sh
@@ -2,6 +2,13 @@
 
 echo "Fire it up"
 
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -sL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+echo "done"
+
 while sleep 1;do tput sc;tput cup 0 $(($(tput cols)-38));echo `date` ${SECONDS};tput rc;done
 
 echo "I got here again, again"
