7d1aebddb46058e5a746cc0ce1ed17fd85921b37
Kyle Brennan <kylos101@gmail.com>
2022-03-02 21:12:27 +0000

    Slight tweak

--- init.sh
+++ init.sh
@@ -2,15 +2,15 @@
 
 echo "Fire it up"
 
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -sL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo curl -o /usr/local/bin/docker-compose -fsSL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-$(uname -m) \
+&& sudo chmod +x /usr/local/bin/docker-compose && sudo mkdir -p /usr/local/lib/docker/cli-plugins \
+&& sudo ln -s /usr/local/bin/docker-compose /usr/local/lib/docker/cli-plugins/docker-compose
 
 echo "done"
 
-docker-compose pull
-docker-compose up --wait
-docker-compose down
+docker compose pull
+docker compose up --wait
+docker compose down
+docker compose ls
 
 echo "I got here again, again more, and again"
