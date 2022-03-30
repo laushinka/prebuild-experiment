7285822e8d97b85e906286a6df983c47d147474d
Kyle Brennan <kylos101@gmail.com>
2022-03-30 17:16:25 +0000

    Make it prebuild

---
+++ load-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+for i in {81..120}
+do
+    git checkout -b kyleb/"$i"-prebuild
+    echo "echo another test $i" >> init.sh
+    git add .
+    git commit -m "$i another test"
+    git push --set-upstream origin kyleb/"$i"-prebuild
+done
