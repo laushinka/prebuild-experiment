ec217689d869b6e8098d3674088f4f4a2040031c
Kyle Brennan <kylos101@gmail.com>
2022-04-22 20:26:04 +0000

    backup some stuff

--- load-test.sh
+++ load-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
-for i in {2..21}
+for i in {2..42}
 do
     git checkout -b kyleb/"$i"-prebuild
     echo "echo another test $i" >> init.sh
+    dd if=/dev/zero of=upload_test bs=1M count=1000
     git add .
     git commit -m "$i another test"
     git push --set-upstream origin kyleb/"$i"-prebuild
